f342aa5563c879b3e045d495b9b42717e8f002c9
Administrator <1145265355@qq.com>
2019-01-14 08:44:33 +0800

    first

--- itripauth/src/main/java/cn/itrip/auth/controller/LoginController.java
+++ itripauth/src/main/java/cn/itrip/auth/controller/LoginController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.security.util.Password;
 
 import java.util.Calendar;
 
